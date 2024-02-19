8d180a213d2b04d5c6e5e716269c024a8de91b5c
xien <2383759126@qq.com>
2024-02-20 01:43:59 +0800

    添加项目文件。

---
+++ 限时无敌/Configuration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using TShockAPI;
+
+namespace InvincibilityPlugin
+{
+    public class Configuration
+    {
+        public static readonly string FilePath = Path.Combine(TShock.SavePath, "限时无敌.json");
+        [JsonProperty("插件自带无敌开启提示")]
+        public bool EnableInvincibleReminder { get; set; } = true;
+
+        [JsonProperty("无敌开启提示自定义文本")]
+        public string CustomInvincibleReminderText { get; set; } = "";
+
+        [JsonProperty("无敌关闭提示自定义文本")]
+        public string CustomInvincibleDisableText { get; set; } = "你的无敌已关闭！";
+
+        public void Write(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                var str = JsonConvert.SerializeObject(this, Formatting.Indented);
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.Write(str);
+                }
+            }
+        }
+
+        public static Configuration Read(string path)
+        {
+            if (!File.Exists(path))
+                return new Configuration();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sr = new StreamReader(fs))
+                {
+                    var cf = JsonConvert.DeserializeObject<Configuration>(sr.ReadToEnd());
+                    return cf;
+                }
+            }
+        }
+    }
+}
---
+++ 限时无敌/InvincibilityPlugin.cs
@@ -0,0 +1,131 @@
+﻿using TerrariaApi.Server;
+using TShockAPI;
+using Terraria;
+using Terraria.GameContent.Creative;
+using Google.Protobuf.WellKnownTypes;
+using System.Configuration;
+using TShockAPI.Hooks;
+
+namespace InvincibilityPlugin
+{
+    [ApiVersion(2, 1)]
+    public class InvincibilityPlugin : TerrariaPlugin
+    {
+        private Timer invincibleTimer;
+        private float duration;
+        public override string Author => "肝帝熙恩";
+        public override string Description => "在命令中给予玩家一段时间的无敌状态。";
+        public override string Name => "InvincibilityPlugin";
+        public override Version Version => new Version(1, 0, 0, 1);
+        public static Configuration Config;
+        public InvincibilityPlugin(Main game) : base(game) 
+        {
+            LoadConfig();
+        }
+
+        private static void LoadConfig()
+        {
+            Config = Configuration.Read(Configuration.FilePath);
+            Config.Write(Configuration.FilePath);
+
+        }
+        private static void ReloadConfig(ReloadEventArgs args)
+        {
+            LoadConfig();
+            args.Player?.SendSuccessMessage("[{0}] 重新加载配置完毕。", typeof(InvincibilityPlugin).Name);
+        }
+        public override void Initialize()
+        {
+            GeneralHooks.ReloadEvent += ReloadConfig;
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("限时无敌", InvincibleCommand, "tempinv", "限时无敌"));
+        }
+
+        private void InvincibleCommand(CommandArgs args)
+        {
+            // 检查权限
+            if (!args.Player.HasPermission("限时无敌"))
+            {
+                args.Player.SendErrorMessage("你没有执行此命令的权限。");
+                return;
+            }
+
+            if (args.Parameters.Count < 1)
+            {
+                args.Player.SendErrorMessage("用法: /限时无敌或tempinv <持续时间秒数>");
+                return;
+            }
+
+            if (!float.TryParse(args.Parameters[0], out duration) || duration <= 0)
+            {
+                args.Player.SendErrorMessage("无效的持续时间。请输入一个正数。");
+                return;
+            }
+
+            // 获取玩家
+            TSPlayer player = TShock.Players[args.Player.Index];
+
+            // 检查玩家是否在线
+            if (player == null || !player.Active)
+            {
+                args.Player.SendErrorMessage("玩家不在线。");
+                return;
+            }
+
+            // 赋予无敌状态
+            addGodMode(player);
+
+            // 设置计时器，在指定的持续时间后移除无敌状态
+            invincibleTimer = new Timer(RemoveInvincibility, player, (int)(duration * 1000), Timeout.Infinite);
+        }
+
+
+
+        private void RemoveInvincibility(object state)
+        {
+            TSPlayer player = (TSPlayer)state;
+            delGodMode(player);
+            invincibleTimer.Dispose(); // 释放计时器资源
+        }
+
+        private void addGodMode(TSPlayer player)
+        {
+            player.GodMode = !player.GodMode;
+            CreativePowerManager.Instance.GetPower<CreativePowers.GodmodePower>().SetEnabledState(player.Index, player.GodMode);
+            if (Config.EnableInvincibleReminder == true)
+            {
+                player.SendSuccessMessage($"你将在 {duration} 秒内无敌.");
+            }
+            else
+            {
+                player.SendSuccessMessage($"{Config.CustomInvincibleReminderText}");
+            }
+
+            // 发送数据包同步无敌状态
+            NetMessage.SendData((int)PacketTypes.PlayerInfo, -1, -1, null, player.Index, 1f);
+        }
+
+        private void delGodMode(TSPlayer player)
+        {
+            player.GodMode = !player.GodMode;
+            CreativePowerManager.Instance.GetPower<CreativePowers.GodmodePower>().SetEnabledState(player.Index, player.GodMode);
+            player.SendSuccessMessage($"{Config.CustomInvincibleDisableText}");
+
+            // 发送数据包同步无敌状态
+            NetMessage.SendData((int)PacketTypes.PlayerInfo, -1, -1, null, player.Index, 1f);
+        }
+    }
+}
